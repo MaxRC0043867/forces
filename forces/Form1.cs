776d787fd0d52c7d3ed6518131940afaca3a472e
Max Welsby West <RC0043867@student.riversidecollege.ac.uk>
2021-05-24 15:51:21 +0100

    jhft

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -20,5 +20,32 @@ namespace forces
         {
 
         }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        }
+        double Cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.acos(x) * 180 / Math.PI);
+        }
+        {
+        double Tan(double x)
+        {
+            return (Math.Tan(x * Math.PI / 180.0));
+        }
+        double atan(double x)
+        {
+            return (Math.Atan(x) * 180 / Math.PI);
+        }
+
     }
 }
